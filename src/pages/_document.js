76ca1c524ac86be8c7fec379f3fe8ec321fd741e
DarellYuhu <s2200064@student.unklab.ac.id>
2023-08-31 16:40:04 +0800

    fix: next script

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,4 +1,5 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 export default function Document() {
   return (
@@ -8,7 +9,7 @@ export default function Document() {
         <Main />
         <NextScript />
       </body>
-      <script src="https://unpkg.com/ionicons@latest/dist/ionicons.js"></script>
+      <Script src="https://unpkg.com/ionicons@latest/dist/ionicons.js"></Script>
     </Html>
   );
 }
