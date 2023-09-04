0807caa33df5b8f5e64f40ed6b6578fcee72774e
DarellYuhu <s2200064@student.unklab.ac.id>
2023-09-04 11:38:34 +0800

    fix: next script error

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,10 @@
 import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <Script src="https://unpkg.com/ionicons@latest/dist/ionicons.js" />
+    </>
+  );
 }
